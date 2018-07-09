c70cc1d3ae689d6929c5c05387c39e8cc924ec26
WillemSchoeman <willemschoeman17@gmail.com>
2018-07-09 21:44:55 +0200

    Worked on the Java backend of the LoginActivity and connected the app to Firebase.

--- app/src/main/java/com/example/shumy/logintest/LoginActivity.java
+++ app/src/main/java/com/example/shumy/logintest/LoginActivity.java
@@ -1,17 +1,33 @@
 package com.example.shumy.logintest;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = "LoginActivity";
 
+    private FirebaseAuth mAuth;
     Button registerButton;
     Button loginButton;
+    EditText emailText;
+    EditText passwordText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,23 +36,69 @@ public class LoginActivity extends AppCompatActivity {
 
         registerButton = findViewById(R.id.register_button);
         loginButton = findViewById(R.id.login_button);
+        emailText = findViewById(R.id.email_text);
+        passwordText = findViewById(R.id.password_text);
 
-        registerButton.setOnClickListener(new View.OnClickListener() {
+        passwordText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
-            public void onClick(View view) {
-
-                Intent registerIntent = new Intent();
-
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == 100 || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
             }
         });
+        mAuth = FirebaseAuth.getInstance();
+    }
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
+    // Executed when Sign in button pressed
+
+    public void signInExistingUser(View v)   {
+        attemptLogin();
+    }
+
+    // Executed when Register button pressed
+
+    public void registerNewUser(View v) {
+        Intent registerIntent = new Intent(this, RegisterActivity.class);
+        finish();
+        startActivity(registerIntent);
+    }
+    private void attemptLogin() {
+
+        String email = emailText.getText().toString();
+        String password = passwordText.getText().toString();
+
+        if (email.isEmpty())
+            if (email.equals("") || password.equals("")) return;
+        Toast.makeText(this, "Login in progress...", Toast.LENGTH_SHORT).show();
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
-            public void onClick(View view) {
+            public void onComplete(@NonNull Task<AuthResult> task) {
 
-//                Intent loginIntent = new Intent();
+                Log.d(TAG, "signInWithEmail() onComplete: " + task.isSuccessful());
+
+                if (!task.isSuccessful()) {
+                    Log.d(TAG, "Problem Signing in: " + task.getException());
+                    showErrorDialog("There was a problem signing in");
+                } else {
+                    Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
+                    finish();
+                    startActivity(loginIntent);
+                }
             }
         });
+    }
 
+    private void showErrorDialog(String message) {
+
+        new AlertDialog.Builder(this)
+                .setTitle("Oops")
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
     }
 }
